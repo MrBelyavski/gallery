5710d54c4037cd186ac48cd240bf7269c2d105d4
MrBelyavski <mikhailbel5628@gmail.com>
2025-06-18 15:27:04 +0300

    Добавлена возможность загрузки изображений по запросу в поисковой строке

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-async function loadImages() {
-  const accessKey = '';
-  const response = await fetch(`https://api.unsplash.com/photos/random?count=12&client_id=${accessKey}`);
-  const images = await response.json();
+const accessKey = '';
+
+async function loadImages(query = '') {
+  let url = '';
+  if (query) {
+    url = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&per_page=12&client_id=${accessKey}`;
+  } else {
+    url = `https://api.unsplash.com/photos/random?count=12&client_id=${accessKey}`;
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const images = query ? data.results : data;
+
   const gallery = document.querySelector('.gallery-grid');
+  gallery.innerHTML = '';
   images.forEach(img => {
     const imgElem = document.createElement('img');
     imgElem.src = img.urls.small;
@@ -10,4 +21,10 @@ async function loadImages() {
   });
 }
 
-window.onload = loadImages;
+window.onload = () => loadImages();
+
+document.getElementById('search').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const query = document.getElementById('search-input').value.trim();
+  loadImages(query);
+});
